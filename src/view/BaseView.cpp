115f9b2a70a4cc0b520f52e262568d2706e2296d
Kirtsim <filipmistrik@gmail.com>
2020-09-24 12:09:31 +0100

    Rework IView and BaseView to support ncurses

--- src/view/BaseView.cpp
+++ src/view/BaseView.cpp
@@ -40,3 +40,194 @@ void BaseView::println(const std::string& iContent) const
 }
 } // namespace view
 } // namespace memo
+
+#include <ncursesw/curses.h>
+
+namespace memo {
+namespace ui {
+namespace view {
+
+const Border BORDER_DEFAULT {
+    ACS_HLINE, ACS_VLINE, ACS_HLINE, ACS_VLINE,
+    { ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LLCORNER }
+};
+
+BaseView::BaseView(const IView::Ptr& iParent) :
+    BaseView({ LINES, COLS }, { 0, 0 }, iParent)
+{}
+
+BaseView::BaseView(const Size& iSize, const IView::Ptr& iParent) :
+    BaseView(iSize, { 0, 0 }, iParent)
+{}
+
+BaseView::BaseView(const Size& iSize, const Position& iPosition, const IView::Ptr& iParent) :
+    parentView_(iParent),
+    width_(iSize.width), height_(iSize.height),
+    x_(iPosition.x), y_(iPosition.y),
+    border_(BORDER_DEFAULT),
+    visible_(true),
+    window_(newwin(height_, width_, 0, 0))
+{}
+
+BaseView::~BaseView()
+{
+    if (window_)
+    {
+        eraseWindow();
+        auto* window = window_.release();
+        delwin(window);
+    }
+}
+
+void BaseView::saveState()
+{}
+
+void BaseView::refresh()
+{
+    if (!visible_) return;
+
+    //Size parentSize = getParentSize();
+    Position parentPos = getParentPosition();
+    const int y = parentPos.y + y_;
+    const int x = parentPos.x + x_;
+
+    eraseWindow();
+    wresize(window_.get(), height_, width_);
+    mvwin(window_.get(), y, x);
+    box(window_.get(), border_.left, border_.top);
+    wrefresh(window_.get());
+}
+
+Size BaseView::getParentSize() const
+{
+    if (parentView_) parentView_->getSize();
+    return { LINES, COLS }; // ncurses' defined dimensions
+}
+
+Position BaseView::getParentPosition() const
+{
+    if (parentView_) return parentView_->getPosition();
+    return { 0, 0 };
+}
+
+void BaseView::eraseWindow()
+{
+    if (!window_) return;
+    wborder(window_.get(), ' ', ' ', ' ',' ',' ',' ',' ',' ');
+    wrefresh(window_.get());
+    applyBorder();
+}
+
+void BaseView::applyBorder()
+{
+    if (!window_) return;
+    wborder(window_.get(),
+            border_.left, border_.right,
+            border_.top, border_.bottom,
+            border_.corner.upperLeft,
+            border_.corner.upperRight,
+            border_.corner.lowerLeft,
+            border_.corner.lowerRight);
+}
+
+void BaseView::setVisible(bool iVisible)
+{
+    visible_ = iVisible;
+}
+
+bool BaseView::isVisible() const
+{
+    return visible_;
+}
+
+void BaseView::setHeight(int iHeight)
+{
+    height_ = iHeight;
+}
+
+void BaseView::setWidth(int iWidth)
+{
+    width_ = iWidth;
+}
+
+void BaseView::setSize(const Size& iSize)
+{
+    setWidth(iSize.width);
+    setHeight(iSize.height);
+}
+
+int BaseView::getHeight() const
+{
+    return height_;
+}
+
+int BaseView::getWidth() const
+{
+    return width_;
+}
+
+Size BaseView::getSize() const
+{
+    return { width_, height_ };
+}
+
+void BaseView::setPosY(int iY)
+{
+    y_ = iY;
+}
+
+void BaseView::setPosX(int iX)
+{
+    x_ = iX;
+}
+
+void BaseView::setPosition(const Position& iPos)
+{
+    setPosY(iPos.y);
+    setPosX(iPos.x);
+}
+
+int BaseView::getPosY() const
+{
+    return y_;
+}
+
+int BaseView::getPosX() const
+{
+    return x_;
+}
+
+Position BaseView::getPosition() const
+{
+    return { y_, x_ };
+}
+
+void BaseView::setParentView(const IView::Ptr& iParent)
+{
+    parentView_ = iParent;
+}
+
+const IView::Ptr& BaseView::getParentView()
+{
+    return parentView_;
+}
+
+void BaseView::setBorder(const Border& iBorder)
+{
+    border_ = iBorder;
+    applyBorder();
+}
+
+Border BaseView::getBorder() const
+{
+    return border_;
+}
+
+Window_t& BaseView::getWindow()
+{
+    return *window_;
+}
+
+} // namespace view
+} // namespace ui
+} // namespace memo
--- src/view/BaseView.hpp
+++ src/view/BaseView.hpp
@@ -29,3 +29,82 @@ private:
 
 } // namespace view
 } // namespace memo
+
+//#include <ncursesw/curses.h>
+
+struct _win_st;
+
+using Window_t = _win_st;
+using WindowPtr_t = std::unique_ptr<Window_t>;
+
+namespace memo {
+    class Client;
+namespace manager {
+    class ViewManager;
+}
+namespace ui {
+namespace view {
+
+class BaseView : public IView
+{
+public:
+    explicit BaseView(const IView::Ptr& iParent=nullptr);
+    explicit BaseView(const Size& iSize, const IView::Ptr& iParent=nullptr);
+    BaseView(const Size& iSize, const Position& iPosition, const IView::Ptr& iParent=nullptr);
+
+    virtual ~BaseView();
+    BaseView(const BaseView&) = delete;
+    BaseView(const BaseView&&) = delete;
+    BaseView& operator=(const BaseView&) = delete;
+
+    void saveState() override;
+    void refresh() override;
+
+    void setVisible(bool iVisible) override;
+    bool isVisible() const override;
+
+    void setHeight(int iHeight) override;
+    void setWidth(int iWidth) override;
+    void setSize(const Size& iSize) override;
+    int getHeight() const override;
+    int getWidth() const override;
+    Size getSize() const override;
+
+    void setPosY(int iY) override;
+    void setPosX(int iX) override;
+    void setPosition(const Position& iPos) override;
+    int getPosY() const override;
+    int getPosX() const override;
+    Position getPosition() const override;
+
+    void setParentView(const IView::Ptr& iParent) override;
+    const IView::Ptr& getParentView() override;
+
+    void setBorder(const Border& iBorder) override;
+    Border getBorder() const override;
+
+/// *** IView methods not imlemented ***
+//  void focus();
+
+protected:
+    void eraseWindow();
+    Size getParentSize() const;
+    Position getParentPosition() const;
+    Window_t& getWindow();
+
+private:
+    void applyBorder();
+
+private:
+    IView::Ptr parentView_;
+    int width_, height_;
+    int x_, y_;
+    Border border_;
+
+    bool visible_;
+    WindowPtr_t window_;
+};
+
+} // namespace view
+} // namespace ui
+} // namespace memo
