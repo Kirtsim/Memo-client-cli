8f0f056f0ac6990caf1f50e62a2d85617ae02891
Kirtsim <filipmistrik@gmail.com>
2020-09-25 15:09:09 +0100

    create a Tools class

---
+++ src/view/tools/Tools.cpp
@@ -0,0 +1,43 @@
+#include "view/tools/Tools.hpp"
+#include "view/IComponent.hpp"
+
+namespace memo {
+namespace ui {
+namespace tools {
+
+void Tools::centerComponent(ui::IComponent& ioComponent, CenterType centerType,
+                            const ui::IComponent& iParent)
+{
+    const Bounds& bounds = componentBoundaries(iParent);
+    centerComponent(ioComponent, centerType, bounds);
+}
+
+void Tools::centerComponent(ui::IComponent& ioComponent, CenterType centerType,
+                            const Bounds& bounds)
+{
+    if (centerType & CenterType::HORIZONTAL)
+    {
+        int newX = (bounds.startX + bounds.endX - ioComponent.getWidth()) / 2;
+        ioComponent.setX(newX);
+    }
+
+    if (centerType & CenterType::VERTICAL)
+    {
+        int newY = (bounds.startY + bounds.endY - ioComponent.getHeight()) / 2;
+        ioComponent.setY(newY);
+    }
+}
+
+Bounds Tools::componentBoundaries(const ui::IComponent& iComponent)
+{
+    Bounds bounds;
+    bounds.startX = iComponent.getX();
+    bounds.startY = iComponent.getY();
+    bounds.endX = bounds.startX + iComponent.getWidth();
+    bounds.endY = bounds.startY + iComponent.getHeight();
+    return bounds;
+}
+
+} // namespace tools
+} // namespace ui
+} // namespace memo
