eeb7b5b733c94a616f53fc1b0b6e23697be1263b
Filip Mistrik <filipmistrik@gmail.com>
2021-05-13 19:12:56 +0100

    Update function description

--- src/view/tools/StringTools.hpp
+++ src/view/tools/StringTools.hpp
@@ -7,14 +7,16 @@ namespace memo::tools {
 
 /// Splits the given text into lines. A section of text is split into two lines if:
 /// a) There is a line feed (\n)
-/// b) The line does not fit into the maxLineLen and must be further split (wrapped on a new line)
+/// b) Word wrap is enabled and the line does not fit into the maxLineLen and must be further split
+///    (wrapped on a new line)
 ///
-/// If maxLineLen is 0 then condition b) is not taken into account, i.e. no line/word wrap.
-/// Empty text produces a single (empty) line. An extra empty line is added to the end in a scenario
-/// when the last line has the same length as the maxLineLen.
+/// If maxLineLen is 0 then condition b) is not taken into account, i.e. no line/word wrap. The
+/// text is then split based on condition a), disregarding the maximum line length (0).
+/// Empty text produces a single (empty) line.
 ///
 /// \param text The text to be split into lines.
 /// \param maxLineLen The maximum allowed length for a line.
+/// \param wordWrap Flag indicating whether word wrapping should be applied
 /// \return A vector of lines (strings).
 std::vector<std::string> splitIntoLines(const std::string& text, size_t maxLineLen, bool wordWrap=false);
 
