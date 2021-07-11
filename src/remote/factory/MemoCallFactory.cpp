2a93cd16a4e450aba454241651995f206528b5b4
Filip Mistrik <filipmistrik@gmail.com>
2021-07-11 19:54:12 +0100

    Update MemoCallFactory with new service calls.

--- src/remote/factory/MemoCallFactory.cpp
+++ src/remote/factory/MemoCallFactory.cpp
@@ -1,11 +1,15 @@
 #include "remote/factory/MemoCallFactory.hpp"
 #include "remote/ListMemoCall.hpp"
+#include "remote/ListMemosCall.hpp"
 #include "remote/MemoCreateCall.hpp"
+#include "remote/AddMemoCall.hpp"
 
 namespace memo::remote {
 
-MemoCallFactory::MemoCallFactory(const MemoSvcStubPtr_t& memoStub)
+MemoCallFactory::MemoCallFactory(const MemoSvcStubPtr_t& memoStub,
+                                 const MemoServiceStubPtr_t& memoServiceStub)
     : stub_(memoStub)
+    , memoServiceStub_(memoServiceStub)
 {
 }
 
@@ -20,6 +24,20 @@ std::shared_ptr<MemoCreateCall> MemoCallFactory::createMemoCreateCall()
 {
     if (stub_)
         return std::make_shared<MemoCreateCall>(*stub_);
+    return nullptr;
+}
+
+std::shared_ptr<ListMemosCall> MemoCallFactory::createListMemosCall()
+{
+    if (memoServiceStub_)
+        return std::make_shared<ListMemosCall>(*memoServiceStub_);
+    return nullptr;
+}
+
+std::shared_ptr<AddMemoCall> MemoCallFactory::createAddMemoCall()
+{
+    if (memoServiceStub_)
+        return std::make_shared<AddMemoCall>(*memoServiceStub_);
     return nullptr;
 }
 
--- src/remote/factory/MemoCallFactory.hpp
+++ src/remote/factory/MemoCallFactory.hpp
@@ -5,19 +5,28 @@
 namespace memo::remote {
 
 class ListMemoCall;
+class ListMemosCall;
 class MemoCreateCall;
+class AddMemoCall;
 using MemoSvcStubPtr_t = std::shared_ptr<proto::MemoSvc::Stub>;
+using MemoServiceStubPtr_t = std::shared_ptr<proto::MemoService::Stub>;
 
 class MemoCallFactory
 {
 public:
-    explicit MemoCallFactory(const MemoSvcStubPtr_t& memoStub);
+    MemoCallFactory(const MemoSvcStubPtr_t& memoStub,
+                    const MemoServiceStubPtr_t& memoServiceStub);
 
     std::shared_ptr<ListMemoCall> createMemoSearchCall();
 
     std::shared_ptr<MemoCreateCall> createMemoCreateCall();
 
+    std::shared_ptr<ListMemosCall> createListMemosCall();
+
+    std::shared_ptr<AddMemoCall> createAddMemoCall();
+
 private:
     MemoSvcStubPtr_t stub_;
+    MemoServiceStubPtr_t memoServiceStub_;
 };
 } // namespace memo::remote
