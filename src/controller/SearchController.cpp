46f9a7ef2d3ff38dbb2a358de333188683e943c9
Filip Mistrik <filipmistrik@gmail.com>
2021-07-12 21:46:23 +0100

    User new MemoService in the SearchController for listing memos.

--- src/controller/SearchController.cpp
+++ src/controller/SearchController.cpp
@@ -2,9 +2,18 @@
 #include "ncurses/functions.hpp"
 #include "ncurses/keys.hpp"
 #include "remote/MemoDao.hpp"
+#include "remote/MemoService.hpp"
+#include "remote/ListMemoRequest.hpp"
+#include "remote/ServiceResponse.hpp"
+#include "remote/ListMemoResponseData.hpp"
+#include "remote/ServiceEnums.hpp"
 #include "view/widget/ListView.hpp"
 
 namespace memo::ctrl {
+
+#ifndef NEW_LIST_MEMOS
+#define NEW_LIST_MEMOS
+#endif
 
 class TestListItem : public ui::ListItem
 {
@@ -28,15 +37,50 @@ SearchController::SearchController(const ResourcesPtr_t& resources)
 {
     auto view = std::make_shared<ui::MemoSearchView>(curses::ScreenSize());
     setView(view);
+    fetchMemos();
+}
+
+
+void SearchController::fetchMemos()
+{
+#ifdef NEW_LIST_MEMOS
+    std::vector<ui::ListItemPtr> memoTitles;
+    auto memoService = getResources()->memoService();
+    if (!memoService)
+    {
+        // TODO: log error + display message
+        return;
+    }
+    remote::ListMemoRequestBuilder requestBuilder;
+    requestBuilder.setRequestId("0000-1111-2222-3333-4444") // TODO: Generate a unique uuid
+            .setMemoView(remote::ModelView::kMinimalView)
+            .setPageSize(20);
+    auto response = memoService->listMemos(requestBuilder.build());
+    if (response && response->status() == remote::ResponseStatus::kSuccess)
+    {
+        for (const auto& memo : response->data().memos())
+        {
+            if (memo)
+                memoTitles.emplace_back(std::make_shared<TestListItem>(memo->title()));
+        }
+    }
+    else
+    {
+        //TODO: log an error + show the user a message that sth went wrong
+    }
+#else
     auto memoDao = getResources()->memoDao();
     auto memos = memoDao->fetchAll();
-    std::vector<ui::ListItemPtr> memoTitles;
     std::transform(memos.begin(), memos.end(), std::back_inserter(memoTitles),
         [](const proto::Memo& memo)
         {
             return std::make_shared<TestListItem>(memo.title());
         });
-    view->memoListView()->setItems(memoTitles);
+#endif
+    if (auto rootView = view())
+    {
+        rootView->memoListView()->setItems(memoTitles);
+    }
 }
 
 void SearchController::processInput()
